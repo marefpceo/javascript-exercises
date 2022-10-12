f8ec5bfd9663974c8212dd82c06e53948bcf33b1
marefpceo <marefpceo@gmail.com>
2022-10-11 21:29:28 -0700

    Completed Lesson 03_reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(stringInput) {
+    let str = stringInput;
+    const stringArray = str.split("");
+
+    let backwardString = stringArray.reverse();
+    return backwardString.join("");
 
 };
 
