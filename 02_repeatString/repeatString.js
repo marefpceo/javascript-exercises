20d965831a3c8f21fb16591bac98ebe6e7c585e4
marefpceo <marefpceo@gmail.com>
2022-10-11 20:57:18 -0700

    Completed Lesson 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatLength = 3;
 
+const repeatString = function(str, repeatLength) {
+    let hey = '';
+    if(repeatLength < 0) {
+        return 'ERROR';
+    }
+    
+    for(let i = 0; i < repeatLength; i++){
+        hey += str;
+    }
+    return hey;
 };
 
 // Do not edit below this line
