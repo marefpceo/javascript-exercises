b06a589877eb760697e4400b7ed827ab204b15eb
marefpceo <marefpceo@gmail.com>
2022-10-12 19:13:08 -0700

    Completed Lesson 04_removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+    let inputArray = arguments[0];   
+    let valuePosition;
+    let modifiedArray;
+
+    for (let i = 1; i < arguments.length; i++){
+        while(inputArray.indexOf(arguments[i]) !== -1){
+            valuePosition = inputArray.indexOf(arguments[i]);
+            modifiedArray = inputArray.splice(valuePosition,1);
+        }
+    }
+    return inputArray;     
 };
 
 // Do not edit below this line
