6cd6efbe0bf96e5a21b8b8445b64ec338c272158
marefpceo <marefpceo@gmail.com>
2022-10-12 20:17:03 -0700

    Completed Lesson 05_sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
 
+    while((num1 > 0) && (num2 > 0) && (typeof(num1) === 'number') && (typeof(num2) === 'number')){
+        if(num2 > num1) {
+            for(let i = num1; i <= num2; i++){
+                total += i;
+            }
+        } else {
+            for(let i = num2; i <= num1; i++){
+                total += i;
+            }
+        }
+        return total;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
