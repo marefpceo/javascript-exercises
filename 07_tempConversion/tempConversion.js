0af8d58478a278c6cd627d0979970285704feda7
marefpceo <marefpceo@gmail.com>
2022-10-12 21:33:04 -0700

    Completed Lesson 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(inputTemp) {
+    let convertedTemp = 0;
 
+    convertedTemp = Math.round(((inputTemp - 32) * (5/9)) * 10)/10;
+    return convertedTemp;
 };
 
-const ctof = function() {
+const ctof = function(inputTemp) {
+    let convertedTemp = 0;
 
+    convertedTemp = Math.round(((inputTemp * (9/5) + 32)) * 10) / 10;
+    return convertedTemp;
 };
 
 // Do not edit below this line
