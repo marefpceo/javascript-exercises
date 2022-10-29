4cc4fcfdf6db4d70351b4b52c6404bf717a1140c
marefpceo <marefpceo@gmail.com>
2022-10-28 21:41:00 -0700

    Completed Calculator lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(input1, input2) {
+	let total = input1 + input2
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(input1, input2) {
+	let subTotal = input1 - input2;
+  return subTotal;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+
+  return input.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
+const multiply = function(input) {
 
+  return input.reduce((a, b) => a*b, 1);
+  
 };
 
-const power = function() {
-	
+const power = function(...input) {
+	return input.reduce((a, b) => a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let inputArray = []; 
+
+  if(input === 0) {
+    return 1;
+  }else {
+    for (let i = 1; i <= input; i++) {
+      inputArray.push(i);
+    }
+  
+    const total = inputArray.reduce((a, b) => a * b, 1);
+    return total;
+  } 
 };
 
 // Do not edit below this line
