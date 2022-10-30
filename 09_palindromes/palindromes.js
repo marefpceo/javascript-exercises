c2b93af277d89c6b4d6f1663123416bf88dba266
marefpceo <marefpceo@gmail.com>
2022-10-30 08:54:15 -0700

    Completed Lesson 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let inputString = input.toLowerCase().replace(/[^A-Za-z]/g, "");
+    let inputArray = inputString.split('');
+    let reverseArray = [...inputArray].reverse();
 
+    let isMatch = "";
+    if(inputArray.length !== reverseArray.length) {
+        return false;
+    }else {
+        for (let i = 0; i < inputArray.length; i++){
+            if(inputArray[i] !== reverseArray[i]){
+                return false;
+            }else {
+                isMatch = true;
+            }
+        }
+        return isMatch;
+    }
 };
 
 // Do not edit below this line
