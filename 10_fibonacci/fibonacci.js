0c272ffb163c212e50c7f90dd3cab326be6ee5b7
marefpceo <marefpceo@gmail.com>
2022-10-31 21:23:52 -0700

    Completed Lesson 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(inputVal) {
+    let input = Number(inputVal);
 
+    if(input < 0){
+        return "OOPS";
+    }else if (input <= 1){
+        return input;
+    }
+
+    const result = [0, 1];
+
+    for (let i = 2; i <= input; i++){
+        result[i] = result[i - 1] + result[i - 2];
+    }
+
+    return result[result.length - 1];
 };
 
 // Do not edit below this line
