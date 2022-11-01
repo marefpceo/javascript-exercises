52e6fafb874b6f37093a90afc9cc2fa741375f93
marefpceo <marefpceo@gmail.com>
2022-11-01 08:47:20 -0700

    Completed Lesson 11 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
 
+    const currentYear = new Date().getFullYear();
+    const sortedArray = input.sort(compare);
+
+    function compare(a, b) {
+
+        const lastAge = ((a.yearOfDeath || currentYear) - a.yearOfBirth);
+        const nextAge = ((b.yearOfDeath || currentYear) - b.yearOfBirth);
+
+        let comparison = 0;
+
+        if(lastAge < nextAge) {
+            comparison = 1;
+        }else if(lastAge > nextAge) {
+            comparison = -1;
+        }
+        return comparison;
+    } 
+    return sortedArray[0];
 };
 
 // Do not edit below this line
